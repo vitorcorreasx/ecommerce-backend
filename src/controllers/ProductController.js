fcd5e323ac66b95f058af7ddd5f1a6d7d3bb0a23
vitorscorrea <vitornbls2000@gmail.com>
2023-04-13 10:50:01 -0300

    adcionado o calculo total

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -2,19 +2,27 @@ module.exports = {
   async get(userId, knex){
     return await knex('user_products')
                 .join('products', 'products.id', 'user_products.product')
-                .select('products.title', 'products.id','products.price','user_products.amount')
+                .select('products.title', 'products.id','products.price','user_products.amount', 'user_products.total')
                 .where('user', userId)
- },
+  },
   async addProduct({userId, productId}, knex){
     const products = await knex('user_products').where({user: userId, product: productId})
+    const productPrice = await knex('products').first('price').where({id: productId})
+    const productAmount = await knex('user_products').first('amount').where({user: userId, product: productId})
+
     if(products != ''){
-      await knex('user_products').where({user: userId, product: productId}).increment('amount', 1)
+      const totalPrice = productPrice.price * (productAmount.amount +1)
+      await knex('user_products').where({user: userId, product: productId}).increment('amount', 1).update('total', totalPrice)
       return
     }
-      await knex('user_products').insert({user: userId, product: productId, amount: 1})
+      await knex('user_products').insert({user: userId, product: productId, amount: 1, total: productPrice.price})
   },
   async removeProduct({userId, productId}, knex) {
-    await knex('user_products').where({user: userId, product: productId}).decrement('amount', 1)
+    const productPrice = await knex('products').first('price').where({id: productId})
+    const productAmount = await knex('user_products').first('amount').where({user: userId, product: productId})
+    const totalPrice = productPrice.price * (productAmount.amount -1)
+    
+    await knex('user_products').where({user: userId, product: productId}).decrement('amount', 1).update('total', totalPrice)
     await knex('user_products').where({user: userId, product: productId, amount: 0}).del()
   }
 }
