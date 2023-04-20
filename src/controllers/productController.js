03665d421d7b1ce8d62c88ac5da50e428569d901
vitorscorrea <vitornbls2000@gmail.com>
2023-04-20 09:44:29 -0300

    correção em codigo repetido

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,32 +1,32 @@
 module.exports = {
-  async get(userId, knex){
+  async get(user, knex){
     return await knex('user_products')
       .join('products', 'products.id', 'user_products.product')
       .select('products.title', 'products.id', 'products.price', 'user_products.amount', 'user_products.total')
-      .where('user', userId);
+      .where('user', user);
   },
-  async addProduct({userId, productId}, knex){
+  async addProduct({user, productId}, knex){
     const productPrice = await knex('products').first('price').where({id: productId});
-    const productAmount = await knex('user_products').first('amount').where({user: userId, product: productId});
+    const productAmount = await knex('user_products').first('amount').where({user, product: productId});
 
     try {
       const totalPrice = productPrice.price * (productAmount.amount +1);
-      await knex('user_products').where({user: userId, product: productId}).increment('amount', 1).update('total', totalPrice); 
-      return await knex('user_products').first('amount').where({user: userId, product: productId});
+      await knex('user_products').where({user, product: productId}).increment('amount', 1).update('total', totalPrice); 
+      return await knex('user_products').first('amount').where({user, product: productId});
     } 
     catch {
-      await knex('user_products').insert({user: userId, product: productId, amount: 1, total: productPrice.price});
-      return await knex('user_products').first('amount').where({user: userId, product: productId});
+      await knex('user_products').insert({user, product: productId, amount: 1, total: productPrice.price});
+      return await knex('user_products').first('amount').where({user, product: productId});
     }
    
   },
-  async removeProduct({userId, productId}, knex) {
+  async removeProduct({user, productId}, knex) {
     const productPrice = await knex('products').first('price').where({id: productId});
-    const productAmount = await knex('user_products').first('amount').where({user: userId, product: productId});
+    const productAmount = await knex('user_products').first('amount').where({user, product: productId});
     const totalPrice = productPrice.price * (productAmount.amount -1);
     
-    await knex('user_products').where({user: userId, product: productId}).decrement('amount', 1).update('total', totalPrice);
-    await knex('user_products').where({user: userId, product: productId, amount: 0}).del();
-    return await knex('user_products').first('amount').where({user: userId, product: productId});
+    await knex('user_products').where({user, product: productId}).decrement('amount', 1).update('total', totalPrice);
+    await knex('user_products').where({user, product: productId, amount: 0}).del();
+    return await knex('user_products').first('amount').where({user, product: productId});
   }
 };
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,22 +2,22 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
   async create({username, password}, {knex}){
-    const checkUser = await knex('users').first('username').where({username: username});
+    const checkUser = await knex('users').first('username').where({username});
     const hashedPassword = await bcrypt.hash(password, 10);
 
     if(!checkUser){
       await knex('users').insert({
-        username: username,
+        username,
         password: hashedPassword
       });
-      return await knex('users').first('id').where({username: username});
+      return await knex('users').first('id').where({username});
     }
   },
   async login({username, password}, {knex}){
-    const validUser = await knex('users').first('username').where({username: username}).select('password');
+    const validUser = await knex('users').first('username').where({username}).select('password');
     const hashPass = await bcrypt.compare(password, validUser.password);
     if(hashPass){
-      return await knex('users').first('username').where({ username: username }).select('id');
+      return await knex('users').first('username').where({ username }).select('id');
     }
   },
 };
