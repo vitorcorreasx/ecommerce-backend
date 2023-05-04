5b6428a4bd1874d14234c57727734cb45682c190
vitorscorrea <vitornbls2000@gmail.com>
2023-05-04 10:50:31 -0300

    :bug: correção no refetch

--- src/graphql/resolvers/mutation/product.js
+++ src/graphql/resolvers/mutation/product.js
@@ -1,22 +1,25 @@
 module.exports = {
   Mutation: {
     addUserProduct: async (_, { userId, input}, {knex}) => {
+      const res = await knex('user_products').first('user_id').where({user_id: userId});
       if(await knex('user_products').where({user_id: userId, product_id: input[0].id}) == '') {
-        return await knex('user_products').insert({
+        await knex('user_products').insert({
           user_id: userId,
           product_id: input[0].id,
           amount: input[0].amount,
           total: input[0].total
         });
+        return res;
       }
       await knex('user_products').where({user_id: userId, product_id: input[0].id}).increment('amount', 1);
+      return res;
     },
     updateUserProducts: async (_, { userId, input}, {knex}) => {
       input.forEach(async (item) => {
         await knex('user_products').where({user_id: userId, product_id: item.id, amount: 0}).del();
         await knex('user_products').where({user_id: userId, product_id: item.id}).update({amount: item.amount, total: item.total});
       });
-      return await knex('users').first('id').where({id: userId});
+      return await knex('user_products').first('user_id').where({user_id: userId});
     }
   }
 };
