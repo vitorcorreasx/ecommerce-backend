55252e011e78e93d76754e16cadff7110b9ca57a
vitorscorrea <vitornbls2000@gmail.com>
2023-05-09 08:48:27 -0300

    :ambulance: correção na mutation para adicionar produtos

--- src/graphql/schemas/resolvers/mutation/product.js
+++ src/graphql/schemas/resolvers/mutation/product.js
@@ -1,7 +1,7 @@
 module.exports = {
   Mutation: {
     addUserProduct: async (_, { userId, input}, {knex}) => {
-      if(await knex('user_products').where({user_id: userId, product_id: input.productId}) == '') {
+      if(await knex('user_products').where({user_id: userId, product_id: input.id}) == '') {
         const [res] =  await knex('user_products').insert({
           user_id: userId,
           product_id: input.productId,
@@ -10,16 +10,15 @@ module.exports = {
         }, ['*']);
         return res;
       }
-      const [res] = await knex('user_products').where({user_id: userId, product_id: input.productId}).increment('amount', 1).returning('*');
+      const [res] = await knex('user_products').where({user_id: userId, product_id: input.id}).increment('amount', 1).returning('*');
       return res;
     },
     updateUserProducts: async (_, { userId, input}, {knex}) => {
-      console.log(input);
       input.forEach(async (item) => {
         await knex('user_products').where({user_id: userId, product_id: item.id, amount: 0}).del();
         await knex('user_products').where({user_id: userId, product_id: item.id}).update({amount: item.amount, total: item.total});
       });
-      return await knex('user_products').first().where({user_id: userId});
+      return await knex('user_products').where({user_id: userId});
     }
   }
 };
